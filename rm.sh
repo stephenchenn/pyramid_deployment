ce23fb292c7bcfaca9e088a357de593e1481a658
stephen <chen.stephen151@gmail.com>
2023-05-29 18:07:22 +0930

    changes

--- rm.sh
+++ rm.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 while true; do
-    output=$(tail -n 1 output.txt)
+    # Get the last line from the file
+    last_line=$(tail -n 1 output.txt)
 
-    if echo "$output" | grep -q "^mergedPyramid/1/"; then
-        rm -r tas_images
+    # Check if the last line starts with any of the specified patterns
+    if [[ $last_line =~ ^(mergedPyramid\/[1-4]\/) ]]; then
+        echo "Matching pattern found: $last_line"
         break
     fi
+
     sleep 300  # Sleep for 5 minutes (300 seconds)
 done
